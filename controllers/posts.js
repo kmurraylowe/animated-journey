a7ba9f6e8c3c766666e1f21d9c5505ff0daf3bd1
Morgan <morgan.moreno25@gmail.com>
2021-04-27 20:38:25 -0500

    fix posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,55 +1,61 @@
-const Post = require('../models/Posts')
+const Post = require('../models/Posts');
 
 module.exports = {
-    getPosts: async (req,res)=>{
-        console.log(req.user)
-        try{
-            const postItems = await Post.find({userId:req.user.id})
-            const itemsLeft = await Post.countDocuments({userId:req.user.id,completed: false})
-            res.render('posts.ejs', {posts: postItems, left: itemsLeft, user: req.user})
-        }catch(err){
-            console.log(err)
-        }
-    },
-    createPost: async (req, res)=>{
-        try{
-            await Post.create({post: req.body.postItem, completed: false, userId: req.user.id})
-            console.log('post has been added!')
-            res.redirect('/posts')
-        }catch(err){
-            console.log(err)
-        }
-    },
-    markComplete: async (req, res)=>{
-        try{
-            await Post.findOneAndUpdate({_id:req.body.postIdFromJSFile},{
-                completed: true
-            })
-            console.log('Marked Complete')
-            res.json('Marked Complete')
-        }catch(err){
-            console.log(err)
-        }
-    },
-    markIncomplete: async (req, res)=>{
-        try{
-            await Post.findOneAndUpdate({_id:req.body.postIdFromJSFile},{
-                completed: false
-            })
-            console.log('Marked Incomplete')
-            res.json('Marked Incomplete')
-        }catch(err){
-            console.log(err)
-        }
-    },
-    deletePost: async (req, res)=>{
-        console.log(req.body.postIdFromJSFile)
-        try{
-            await Post.findOneAndDelete({_id:req.body.postIdFromJSFile})
-            console.log('Deleted Todo')
-            res.json('Deleted It')
-        }catch(err){
-            console.log(err)
-        }
-    }
-}    
+	getPosts: async (req, res) => {
+		console.log(req.user);
+		try {
+			const postItems = await Post.find({ userId: req.user.id });
+			const itemsLeft = await Post.countDocuments({ userId: req.user.id, completed: false });
+			res.render('posts.ejs', { posts: postItems, left: itemsLeft, user: req.user });
+		} catch (err) {
+			console.log(err);
+		}
+	},
+	createPost: async (req, res) => {
+		try {
+			await Post.create({ post: req.body.postItem, completed: false, userId: req.user.id });
+			console.log('post has been added!');
+			res.redirect('/posts');
+		} catch (err) {
+			console.log(err);
+		}
+	},
+	markComplete: async (req, res) => {
+		try {
+			await Post.findOneAndUpdate(
+				{ _id: req.body.postIdFromJSFile },
+				{
+					completed: true,
+				}
+			);
+			console.log('Marked Complete');
+			res.json('Marked Complete');
+		} catch (err) {
+			console.log(err);
+		}
+	},
+	markIncomplete: async (req, res) => {
+		try {
+			await Post.findOneAndUpdate(
+				{ _id: req.body.postIdFromJSFile },
+				{
+					completed: false,
+				}
+			);
+			console.log('Marked Incomplete');
+			res.json('Marked Incomplete');
+		} catch (err) {
+			console.log(err);
+		}
+	},
+	deletePost: async (req, res) => {
+		console.log(req.body.postIdFromJSFile);
+		try {
+			await Post.findOneAndDelete({ _id: req.body.postIdFromJSFile });
+			console.log('Deleted Todo');
+			res.json('Deleted It');
+		} catch (err) {
+			console.log(err);
+		}
+	},
+};
--- models/Posts.js
+++ models/Posts.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const PostSchema = new mongoose.Schema({
-  Post: {
-    type: String,
-    required: true,
-  },
-  completed: {
-    type: Boolean,
-    required: true,
-  },
-  userId: {
-    type: String,
-    required: true
-  }
-})
+	post: {
+		type: String,
+		required: true,
+	},
+	completed: {
+		type: Boolean,
+		required: true,
+	},
+	userId: {
+		type: String,
+		required: true,
+	},
+});
 
-module.exports = mongoose.model('Post', PostSchema)
+module.exports = mongoose.model('Post', PostSchema);
