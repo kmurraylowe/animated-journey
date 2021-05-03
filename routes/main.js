1ba0908b8abbf4ca51346fce8f18b8982c056f28
Morgan <morgan.moreno25@gmail.com>
2021-05-03 17:54:08 -0500

    #5 Fix Get Issue

--- routes/main.js
+++ routes/main.js
@@ -2,9 +2,12 @@ const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/auth') 
 const homeController = require('../controllers/home')
+const profileController = require('../controllers/profile');
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 router.get('/', homeController.getIndex)
+router.get('/profile', ensureAuth, profileController.getProfile)
+
 router.get('/login', authController.getLogin)
 router.post('/login', authController.postLogin)
 router.get('/logout', authController.logout)
