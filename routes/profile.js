f3ad5b18cdaad60362221909e37159d34d1f714e
Kye Lowe <kmurraylowe@gmail.com>
2021-05-11 09:44:26 +1000

    added post to profile, added alittle styling on profile and background, changed modal order so img upload is before caption

--- routes/profile.js
+++ routes/profile.js
@@ -8,6 +8,8 @@ router.get('/:id', profileController.getProfile);
 
 router.post('/posts/updatePost/:postId', upload.single('image'), postController.updatePost);
 
+router.post('/posts/createPost', upload.single("file"), postController.createPost);
+
 router.delete('/posts/deletePost',  postController.deletePost);
 
 module.exports = router;
